c36860af3a954b4bbc4d7af2727f683533233e0b
Steven Chicherchia <schicherchia@aol.com>
2020-10-30 02:55:09 -0400

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+    products:[
+    {
+        _id: '1',
+        name: 'Unroasted Mexican Chiapas 10lb',
+        category: 'Unroasted Coffee',
+        image: '/images/p1.jpg',
+        price: 44.95,
+        origin: 'Mexico',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Unroasted Mexican Chiapas EP, HG 10 pound bag'
+    }, 
+    {
+        _id: '2',
+        name: 'Unroasted Mexican Chiapas 70kg bag',
+        category: 'Unroasted Coffee',
+        image: '/images/p1.jpg',
+        price: 649.95,
+        origin: 'Mexico',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Unroasted Mexican Chiapas EP, HG'
+    }, 
+    {
+        _id: '3',
+        name: 'Roasted Mexican Chiapas 5lb',
+        category: 'Roasted Coffee',
+        image: '/images/p2.jpg',
+        price: 44.95,
+        origin: 'Mexico',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Roasted Mexican Chiapas EP, HG 5 pound bag'
+    }, 
+    {
+        _id: '4',
+        name: 'Ground Mexican Chiapas 3oz, case of 24',
+        category: 'Ground Coffee',
+        image: '/images/p1.jpg',
+        price: 44.95,
+        origin: 'Mexico',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'Roasted Ground Mexican Chiapas for use with 12 cup coffee maker'
+    }, 
+
+    ],
+};
+export default data;
